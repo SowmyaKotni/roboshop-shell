c824136dd57e3624e6fe494949dc3191f3ef9c7b
Sowmya <uvsowmya@gmaill.com>
2023-08-14 20:38:55 +0530

    non functional updates

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,4 @@
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Create Catalogue Service    <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Create Catalogue Service    <<<<<<<<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Create MongoDB Service    <<<<<<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
