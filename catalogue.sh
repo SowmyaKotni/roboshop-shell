97a4208e68032ae3def0082f0505e37a86b582a1
Sowmya <uvsowmya@gmaill.com>
2023-08-14 16:53:30 +0530

    roboshop nonfuctional updates

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,28 @@
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Create Catalogue Service    <<<<<<<<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service >/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Create MongoDB Service    <<<<<<<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo >/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>>>> Install NodeJS Repos    <<<<<<<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Install NodeJS    <<<<<<<<<<<<<<<<\e[0m"
-yum install nodejs -y
+yum install nodejs -y >/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Create Application User    <<<<<<<<<<<<<<<<\e[0m"
-useradd roboshop
+useradd roboshop >/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Create Application Directory    <<<<<<<<<<<<<<<<\e[0m"
-mkdir /app
+mkdir /app >/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Download Application Content    <<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Extract Application Content    <<<<<<<<<<<<<<<<\e[0m"
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip >/tmp/roboshop.log
 cd /app
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Download NodeJS Dependencies    <<<<<<<<<<<<<<<<\e[0m"
-npm install
+npm install >/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Install Mongo Client    <<<<<<<<<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y >/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> Load Catalogue Schema    <<<<<<<<<<<<<<<<\e[0m"
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js >/tmp/roboshop.log
+systemctl daemon-reload >/tmp/roboshop.log
+systemctl enable catalogue >/tmp/roboshop.log
+systemctl restart catalogue >/tmp/roboshop.log
 
