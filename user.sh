78d32c21ec749f7d4fa91b1708456a2dfd47e17a
Sowmya <uvsowmya@gmaill.com>
2023-08-18 10:54:12 +0530

    non functional updates

--- user.sh
+++ user.sh
@@ -1,17 +1,5 @@
-cp user.service /etc/systemd/system/user.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
-yum install mongodb-org-shell -y
-mongo --host mongodb.sowmyakotnidevops.online </app/schema/user.js
+component=user
+schema_type=mongodb
+source common.sh
 
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+func_nodejs
